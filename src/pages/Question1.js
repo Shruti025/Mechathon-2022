6e0e4feff0a5e9890f85656d1d576039ff66ec0a
Shruti025 <shrutigoyal25042001@gmail.com>
2022-04-02 22:13:41 +0530

    Question 1 UI

--- src/pages/Question1.js
+++ src/pages/Question1.js
@@ -32,80 +32,80 @@ const Question1 = () => {
             <h1>Find the EOQ</h1>
             <form className='input-form'>
                 <label
-                    htmlFor="name">Name of product:
-                    <input
-                        type="name"
-                        className="form-control"
-                        id="name"
-                        value={name}
-                        onChange={(e) => { setName(e.target.value) }}
-                    />
-                </label>
+                    htmlFor="name">Name of product:</label>
+                <input
+                    type="name"
+                    className="form-control"
+                    id="name"
+                    value={name}
+                    onChange={(e) => { setName(e.target.value) }}
+                    required
+                />
 
                 <label
-                    htmlFor="cost">Cost of product:
-                    <input
-                        type="name"
-                        className="form-control"
-                        id="cost"
-                        value={cost}
-                        onChange={(e) => { setCost(e.target.value) }}
-                    />
-                </label>
+                    htmlFor="cost">Cost of product:</label>
+                <input
+                    type="name"
+                    className="form-control"
+                    id="cost"
+                    value={cost}
+                    onChange={(e) => { setCost(e.target.value) }}
+                    required
+                />
 
-                <label htmlFor="quantity">Quantity:
-                    <input
-                        type="name"
-                        className="form-control"
-                        id="quantity"
-                        value={quantity}
-                        onChange={(e) => { setQuantity(e.target.value) }}
-                    />
-                </label>
-
-                <label
-                    htmlFor="order-cost">Ordering Cost:
-                    <input
-                        type="name"
-                        className="form-control"
-                        id="order-cost"
-                        value={orderCost}
-                        onChange={(e) => { setOrderCost(e.target.value) }}
-                    />
-                </label>
+                <label htmlFor="quantity">Quantity: </label>
+                <input
+                    type="name"
+                    className="form-control"
+                    id="quantity"
+                    value={quantity}
+                    onChange={(e) => { setQuantity(e.target.value) }}
+                    required
+                />
 
                 <label
-                    htmlFor="carry-cost">Carrying Cost:
-                    <input
-                        type="name"
-                        className="form-control"
-                        id="carry-cost"
-                        value={carryCost}
-                        onChange={(e) => { setCarryCost(e.target.value) }}
-                    />
-                </label>
+                    htmlFor="order-cost">Ordering Cost: </label>
+                <input
+                    type="name"
+                    className="form-control"
+                    id="order-cost"
+                    value={orderCost}
+                    onChange={(e) => { setOrderCost(e.target.value) }}
+                    required
+                />
 
                 <label
-                    htmlFor="interest">Interest rate per year:
-                    <input
-                        type="name"
-                        className="form-control"
-                        id="interest"
-                        value={interest}
-                        onChange={(e) => { setInterest(e.target.value) }}
-                    />
-                </label>
+                    htmlFor="carry-cost">Carrying Cost: </label>
+                <input
+                    type="name"
+                    className="form-control"
+                    id="carry-cost"
+                    value={carryCost}
+                    onChange={(e) => { setCarryCost(e.target.value) }}
+                    required
+                />
 
                 <label
-                    htmlFor="prod-runs">Number of production runs per year:
-                    <input
-                        type="name"
-                        className="form-control"
-                        id="prod-runs"
-                        value={prodRun}
-                        onChange={(e) => { setProdRun(e.target.value) }}
-                    />
-                </label>
+                    htmlFor="interest">Interest rate per year: </label>
+                <input
+                    type="name"
+                    className="form-control"
+                    id="interest"
+                    value={interest}
+                    onChange={(e) => { setInterest(e.target.value) }}
+                    required
+                />
+
+                <label
+                    htmlFor="prod-runs">Number of production runs per year: </label>
+                <input
+                    type="name"
+                    className="form-control"
+                    id="prod-runs"
+                    value={prodRun}
+                    onChange={(e) => { setProdRun(e.target.value) }}
+                    required
+                />
 
                 <button onClick={GetValue}>Submit</button>
             </form>
